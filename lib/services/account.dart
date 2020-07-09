8205c12cc90da83c340aed1ea13da95c24c7dea0
Eldad Fux <eldad.fux@gmail.com>
2020-07-09 17:46:59 +0300

    Fix for OAuth2 cookie

--- lib/services/account.dart
+++ lib/services/account.dart
@@ -13,9 +13,6 @@ class Account extends Service {
     Account(Client client): super(client);
 
      /// Get Account
-     ///
-     /// Get currently logged in user data as JSON object.
-     ///
     Future<Response> get() {
         final String path = '/account';
 
@@ -30,14 +27,6 @@ class Account extends Service {
     }
 
      /// Create Account
-     ///
-     /// Use this endpoint to allow a new user to register a new account in your
-     /// project. After the user registration completes successfully, you can use
-     /// the [/account/verfication](/docs/client/account#createVerification) route
-     /// to start verifying the user email address. To allow your new user to login
-     /// to his new account, you need to create a new [account
-     /// session](/docs/client/account#createSession).
-     ///
     Future<Response> create({@required String email, @required String password, String name = ''}) {
         final String path = '/account';
 
@@ -55,13 +44,6 @@ class Account extends Service {
     }
 
      /// Delete Account
-     ///
-     /// Delete a currently logged in user account. Behind the scene, the user
-     /// record is not deleted but permanently blocked from any access. This is done
-     /// to avoid deleted accounts being overtaken by new users with the same email
-     /// address. Any user-related resources like documents or storage files should
-     /// be deleted separately.
-     ///
     Future<Response> delete() {
         final String path = '/account';
 
@@ -76,12 +58,6 @@ class Account extends Service {
     }
 
      /// Update Account Email
-     ///
-     /// Update currently logged in user account email address. After changing user
-     /// address, user confirmation status is being reset and a new confirmation
-     /// mail is sent. For security measures, user password is required to complete
-     /// this request.
-     ///
     Future<Response> updateEmail({@required String email, @required String password}) {
         final String path = '/account/email';
 
@@ -98,10 +74,6 @@ class Account extends Service {
     }
 
      /// Get Account Logs
-     ///
-     /// Get currently logged in user list of latest security activity logs. Each
-     /// log returns user IP address, location and date and time of log.
-     ///
     Future<Response> getLogs() {
         final String path = '/account/logs';
 
@@ -116,9 +88,6 @@ class Account extends Service {
     }
 
      /// Update Account Name
-     ///
-     /// Update currently logged in user account name.
-     ///
     Future<Response> updateName({@required String name}) {
         final String path = '/account/name';
 
@@ -134,10 +103,6 @@ class Account extends Service {
     }
 
      /// Update Account Password
-     ///
-     /// Update currently logged in user password. For validation, user is required
-     /// to pass the password twice.
-     ///
     Future<Response> updatePassword({@required String password, @required String oldPassword}) {
         final String path = '/account/password';
 
@@ -154,9 +119,6 @@ class Account extends Service {
     }
 
      /// Get Account Preferences
-     ///
-     /// Get currently logged in user preferences as a key-value object.
-     ///
     Future<Response> getPrefs() {
         final String path = '/account/prefs';
 
@@ -171,10 +133,6 @@ class Account extends Service {
     }
 
      /// Update Account Preferences
-     ///
-     /// Update currently logged in user account preferences. You can pass only the
-     /// specific settings you wish to update.
-     ///
     Future<Response> updatePrefs({@required dynamic prefs}) {
         final String path = '/account/prefs';
 
@@ -190,14 +148,6 @@ class Account extends Service {
     }
 
      /// Create Password Recovery
-     ///
-     /// Sends the user an email with a temporary secret key for password reset.
-     /// When the user clicks the confirmation link he is redirected back to your
-     /// app password reset URL with the secret key and email address values
-     /// attached to the URL query string. Use the query string params to submit a
-     /// request to the [PUT /account/recovery](/docs/client/account#updateRecovery)
-     /// endpoint to complete the process.
-     ///
     Future<Response> createRecovery({@required String email, @required String url}) {
         final String path = '/account/recovery';
 
@@ -214,17 +164,6 @@ class Account extends Service {
     }
 
      /// Complete Password Recovery
-     ///
-     /// Use this endpoint to complete the user account password reset. Both the
-     /// **userId** and **secret** arguments will be passed as query parameters to
-     /// the redirect URL you have provided when sending your request to the [POST
-     /// /account/recovery](/docs/client/account#createRecovery) endpoint.
-     /// 
-     /// Please note that in order to avoid a [Redirect
-     /// Attack](https://github.com/OWASP/CheatSheetSeries/blob/master/cheatsheets/Unvalidated_Redirects_and_Forwards_Cheat_Sheet.md)
-     /// the only valid redirect URLs are the ones from domains you have set when
-     /// adding your platforms in the console interface.
-     ///
     Future<Response> updateRecovery({@required String userId, @required String secret, @required String password, @required String passwordAgain}) {
         final String path = '/account/recovery';
 
@@ -243,10 +182,6 @@ class Account extends Service {
     }
 
      /// Get Account Sessions
-     ///
-     /// Get currently logged in user list of active sessions across different
-     /// devices.
-     ///
     Future<Response> getSessions() {
         final String path = '/account/sessions';
 
@@ -261,10 +196,6 @@ class Account extends Service {
     }
 
      /// Create Account Session
-     ///
-     /// Allow the user to login into his account by providing a valid email and
-     /// password combination. This route will create a new session for the user.
-     ///
     Future<Response> createSession({@required String email, @required String password}) {
         final String path = '/account/sessions';
 
@@ -281,10 +212,6 @@ class Account extends Service {
     }
 
      /// Delete All Account Sessions
-     ///
-     /// Delete all sessions from the user account and remove any sessions cookies
-     /// from the end client.
-     ///
     Future<Response> deleteSessions() {
         final String path = '/account/sessions';
 
@@ -299,13 +226,7 @@ class Account extends Service {
     }
 
      /// Create Account Session with OAuth2
-     ///
-     /// Allow the user to login to his account using the OAuth2 provider of his
-     /// choice. Each OAuth2 provider should be enabled from the Appwrite console
-     /// first. Use the success and failure arguments to provide a redirect URL's
-     /// back to your app when login is completed.
-     ///
-    Future createOAuth2Session({@required String provider, String success = 'https://appwrite.io/auth/oauth2/success', String failure = 'https://appwrite.io/auth/oauth2/failure', List scopes = const []}) {
+    Future createOAuth2Session({@required String provider, String success = '/auth/oauth2/success', String failure = '/auth/oauth2/failure', List scopes = const []}) {
         final String path = '/account/sessions/oauth2/{provider}'.replaceAll(RegExp('{provider}'), provider);
 
         final Map<String, dynamic> params = {
@@ -349,11 +270,6 @@ class Account extends Service {
     }
 
      /// Delete Account Session
-     ///
-     /// Use this endpoint to log out the currently logged in user from all his
-     /// account sessions across all his different devices. When using the option id
-     /// argument, only the session unique ID provider will be deleted.
-     ///
     Future<Response> deleteSession({@required String sessionId}) {
         final String path = '/account/sessions/{sessionId}'.replaceAll(RegExp('{sessionId}'), sessionId);
 
@@ -368,21 +284,6 @@ class Account extends Service {
     }
 
      /// Create Email Verification
-     ///
-     /// Use this endpoint to send a verification message to your user email address
-     /// to confirm they are the valid owners of that address. Both the **userId**
-     /// and **secret** arguments will be passed as query parameters to the URL you
-     /// have provider to be attached to the verification email. The provided URL
-     /// should redirect the user back for your app and allow you to complete the
-     /// verification process by verifying both the **userId** and **secret**
-     /// parameters. Learn more about how to [complete the verification
-     /// process](/docs/client/account#updateAccountVerification). 
-     /// 
-     /// Please note that in order to avoid a [Redirect
-     /// Attack](https://github.com/OWASP/CheatSheetSeries/blob/master/cheatsheets/Unvalidated_Redirects_and_Forwards_Cheat_Sheet.md)
-     /// the only valid redirect URLs are the ones from domains you have set when
-     /// adding your platforms in the console interface.
-     ///
     Future<Response> createVerification({@required String url}) {
         final String path = '/account/verification';
 
@@ -398,12 +299,6 @@ class Account extends Service {
     }
 
      /// Complete Email Verification
-     ///
-     /// Use this endpoint to complete the user email verification process. Use both
-     /// the **userId** and **secret** parameters that were attached to your app URL
-     /// to verify the user email ownership. If confirmed this route will return a
-     /// 200 status code.
-     ///
     Future<Response> updateVerification({@required String userId, @required String secret}) {
         final String path = '/account/verification';
 
--- lib/services/locale.dart
+++ lib/services/locale.dart
@@ -11,14 +11,6 @@ class Locale extends Service {
     Locale(Client client): super(client);
 
      /// Get User Locale
-     ///
-     /// Get the current user location based on IP. Returns an object with user
-     /// country code, country name, continent name, continent code, ip address and
-     /// suggested currency. You can use the locale header to get the data in a
-     /// supported language.
-     /// 
-     /// ([IP Geolocation by DB-IP](https://db-ip.com))
-     ///
     Future<Response> get() {
         final String path = '/locale';
 
@@ -33,10 +25,6 @@ class Locale extends Service {
     }
 
      /// List Continents
-     ///
-     /// List of all continents. You can use the locale header to get the data in a
-     /// supported language.
-     ///
     Future<Response> getContinents() {
         final String path = '/locale/continents';
 
@@ -51,10 +39,6 @@ class Locale extends Service {
     }
 
      /// List Countries
-     ///
-     /// List of all countries. You can use the locale header to get the data in a
-     /// supported language.
-     ///
     Future<Response> getCountries() {
         final String path = '/locale/countries';
 
@@ -69,10 +53,6 @@ class Locale extends Service {
     }
 
      /// List EU Countries
-     ///
-     /// List of all countries that are currently members of the EU. You can use the
-     /// locale header to get the data in a supported language.
-     ///
     Future<Response> getCountriesEU() {
         final String path = '/locale/countries/eu';
 
@@ -87,10 +67,6 @@ class Locale extends Service {
     }
 
      /// List Countries Phone Codes
-     ///
-     /// List of all countries phone codes. You can use the locale header to get the
-     /// data in a supported language.
-     ///
     Future<Response> getCountriesPhones() {
         final String path = '/locale/countries/phones';
 
@@ -105,11 +81,6 @@ class Locale extends Service {
     }
 
      /// List Currencies
-     ///
-     /// List of all currencies, including currency symbol, name, plural, and
-     /// decimal digits for all major and minor currencies. You can use the locale
-     /// header to get the data in a supported language.
-     ///
     Future<Response> getCurrencies() {
         final String path = '/locale/currencies';
 
@@ -124,10 +95,6 @@ class Locale extends Service {
     }
 
      /// List Languages
-     ///
-     /// List of all languages classified by ISO 639-1 including 2-letter code, name
-     /// in English, and name in the respective language.
-     ///
     Future<Response> getLanguages() {
         final String path = '/locale/languages';
 
