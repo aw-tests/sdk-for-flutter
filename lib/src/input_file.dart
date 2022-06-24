55136002392a33cac4de0bedc6cebf9874b4f0f4
Damodar Lohani <lohanidamodar@users.noreply.github.com>
2022-06-24 10:33:49 +0545

    fixes

--- lib/src/input_file.dart
+++ lib/src/input_file.dart
@@ -1,21 +1,18 @@
+import 'package:flutter/foundation.dart';
+
 class InputFile {
   late final String? path;
   late final List<int>? bytes;
   final String? filename;
   final String? contentType;
 
-  /// Provide a file using path
-  InputFile({required this.path, this.filename, this.contentType}) {
-    bytes = null;
-  }
-
-  InputFile._internal(this.bytes, {this.filename, this.contentType}) {
-    path = null;
-  }
-
-  /// Provide a file using bytes
-  factory InputFile.fromBytes(
-      {required List<int> bytes, String? filename, String? contentType}) {
-    return InputFile._internal(bytes, filename: filename, contentType: contentType);
+  /// Provide a file, use `path` for IO platforms
+  /// and provide `bytes` for web platform
+  InputFile({this.path, this.filename, this.contentType, this.bytes}) {
+    if (kIsWeb && bytes == null) {
+      throw {{ spec.title | caseUcfirs }}Exception('On web `bytes` is required');
+    } else if(path == null) {
+      throw {{ spec.title | caseUcfirs }}Exception('On IO platforms `path is required`);
+    }
   }
 }
